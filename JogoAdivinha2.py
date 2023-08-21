a39c1e1a92250663ff4da03502c83518fbaf5386
Ana R. Carvalho <137975286+AnaRaquelCarvalho@users.noreply.github.com>
2023-08-21 19:18:37 -0300

    Update JogoAdivinha2.py

--- JogoAdivinha2.py
+++ JogoAdivinha2.py
@@ -1,13 +1,18 @@
-print('='*8, 'Jogo da Adivinhação 2' , '='*8)
-print(' [Vou pensar em um número de 0 a 5. Tente adivinhar...]')
+print('='*16, 'Jogo da Adivinhação 2' , '='*16)
+print('[Vou pensar em um número de 0 a 5. Tente adivinhar...]')
 print('='*56)
 jogador = int(input('Em que número pensei? '))
 from random import randint
 computador = randint(0,5)
-print('='*46)
+
+print('    PROCESSANDO...')
+#Tempo para processar...
+from time import sleep
+sleep(3)
+
 if jogador == computador:
     print('Parabéns! Você Venceu! ')
-    print("="*56)
+    print("="*60)
 else:
     print('Você Errou! Pensei no {} e não no {}!'.format(computador, jogador))
     print('='*16, ' Tente Novamente! ','='*16)
