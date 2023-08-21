99710423e77c76a45112a8b40dfa2792099ea697
Ana R. Carvalho <137975286+AnaRaquelCarvalho@users.noreply.github.com>
2023-08-21 19:01:00 -0300

    Create JogoAdivinha.py
    Tente adivinhar o número que o computador vai pensar.

---
+++ JogoAdivinha.py
@@ -0,0 +1,18 @@
+print('='*18, 'Jogo da Adivinhação' ,'='*18)
+print('[Vou pensar em um número de 0 a 5. Tente adivinhar...]')
+print('='*56)
+import random
+jog = int(input('Em que número pensei? '))
+comput = random.randint(1,5)
+print('      Processando...')
+if jog == comput:
+    print('Parabéns! O número é {}'.format(jog,comput))
+    print('='*56)
+else:
+    print('Você Errou! Eu pensei no {} e não no {}!'.format(comput,jog))   
+    print('='*16, 'Tente Novamente!!!! ', '='*16,'\n')
+
+    
+     
+
+
