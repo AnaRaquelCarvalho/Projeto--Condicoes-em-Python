ac62566f39a6c8b337cd8d274a6894d40a762c75
Ana R. Carvalho <137975286+AnaRaquelCarvalho@users.noreply.github.com>
2023-08-21 23:35:52 -0300

    Update Radar2.py

--- Radar2.py
+++ Radar2.py
@@ -5,10 +5,10 @@ print('='*52)
 
 if 80 >= velocidade:
     print('Velocidade Permitida: {} Km/h'.format(80))
-    print('Velocidade atual do carro: {} km/km.'.format(velocidade))
+    print('Velocidade atual do carro: {:.0f} km/km.'.format(velocidade))
     print('Tenha um bom Dia! Dirija com segurança!')
 else:
-    print('Velocidade atual do carro: {} Km/h'.format(velocidade))
+    print('Velocidade atual do carro: {:.0f} Km/h'.format(velocidade))
     print('Multado! Você excedeu o limite permitido que é de 80 km/h')
     print('Você deve pagar uma multa de R$ {:.2f}'.format(multa))
 print('='*52)
